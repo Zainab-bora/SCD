63deb7d5a70bf9bffc1c71ed382e200d4d04441d
Zainab Kashif <zainabkashif29142005@gmail.com>
2026-03-16 09:22:48 +0500

    first commit

---
+++ mainOOP.java
@@ -0,0 +1,18 @@
+public class mainOOP {
+    
+     public static void main(String[] args) {
+        studentOOP s1 = new studentOOP();
+        
+        s1.name="Zainab";
+        s1.rollno= 11;
+        
+        studentOOP s2 = new studentOOP();
+        
+        s2.name="Bora";
+        s2.rollno= 12;
+
+       s1.displayInfo();
+       s2.displayInfo();
+
+    }
+}
---
+++ studentOOP.java
@@ -0,0 +1,13 @@
+public class studentOOP {
+    
+    String name;
+    int rollno;
+
+    public void displayInfo(){
+
+        System.out.println("Student Name is: "+ name);
+        System.out.println("RollNumber is: "+ rollno);
+
+        System.out.println("-------------------------------------");
+    }
+}
